7b7875ab5467fc33d5080b2698d6a8b2d311a954
Madison Mandarino <madisonmandarino@Madisons-MacBook-Pro.local>
2023-02-01 17:10:09 -0500

    rectangle area 2/1

---
+++ ch01/exercises/examples.py
@@ -0,0 +1,9 @@
+var = 4
+var =input("Enter a number: ")
+#to save the number you can do 
+data = input("Enter a number: ")
+print(data, type(data))
+#print(data +1) is an error
+data = int(data)
+print(data, type(data))
+print(data + 1)
