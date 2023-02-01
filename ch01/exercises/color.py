73a933fed42e8d8ceaf60d47df44618f847b603b
Madison Mandarino <madisonmandarino@Madisons-MacBook-Pro.local>
2023-02-01 17:13:27 -0500

    color 2/1

--- ch01/exercises/color.py
+++ ch01/exercises/color.py
@@ -1,10 +1,5 @@
 colors=[
     "red",
-    "blue",
-    "green",
-    "yellow",
-    "orange",
-    "purple",
     "brown",
     "black",
     "white",
@@ -15,3 +10,5 @@ print("I have", len(colors), "colors to choose from.")
 print(colors[0])
 print(colors[1])
 print(colors[2])
+print(colors[3])
+print(colors[4])
