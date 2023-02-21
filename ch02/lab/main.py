28c20ac8ed51d4cd59947effbaabfffed76cbcac
Madison Mandarino <madisonmandarino@Madisons-MacBook-Pro.local>
2023-02-20 19:10:59 -0500

    guess exercise

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,54 @@
+import random 
+import turtle
+import pygame 
+import math
 
-#Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+#part A
+mike = turtle.Turtle()
+don = turtle.Turtle()
+mike.shape("turtle")
+don.shape("turtle")
+mike.color("red")
+don.color("blue")
+mike.up()
+don.up()
+mike.goto(-100,20)
+don.goto(-100,-20)
+window = turtle.Screen()
+a = random.randrange(1,100)
+b = random.randrange(1,100)
+mike.down()
+don.down()
 
+#race 1 
+mike.forward(a)
+don.forward(b)
+mike.up()
+don.up()
+mike.goto(-100,20)
+don.goto(-100,-20)
+mike.down()
+don.down()
 
-#Part B
+#race 2
+for _ in range(1):
+    mike.forward(a)
+    don.forward(b)
+    print(a)
+    print(b)
+    mike.up()
+    don.up()
+    mike.goto(-100,20)
+    don.goto(-100,-20)
+    mike.down()
+    don.down()
+    mike.clear()
+    don.clear()
+
+window.exitonclick()
+
+#part b
+pygame.init()
+window = pygame.display.set_mode()
+window = fill("green")
+pygame.display.flip()
