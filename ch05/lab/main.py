955a1d35ee4e4466186d520e3fabfe5df4d0f119
Madison Mandarino <madisonmandarino@Madisons-MacBook-Pro.local>
2023-03-21 00:00:04 -0400

    lab :/

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,7 +1,5 @@
 import pygame
 
-
-
 def threenp1(n):
     count = 0
     while n > 1.0:
@@ -25,17 +23,27 @@ def graph_coordinates(threenplus1_iters_dict):
     window = pygame.display.set_mode( )
     window.fill("lavender")
     new_display = pygame.transform.flip(window, False, True)
-    pygame.draw.lines(window,"black", False threenplus1_iters_dict)
+    
     width, height = new_display.get_size()
     new_display = pygame.transform.scale(new_display, (width * 0.5, height * 0.5))
-    window.blit(new_display, (0,0))
-
     max_so_far = 0
+    max_num = 0
+    for (p,z) in threenplus1_iters_dict:
+        if z > max_so_far:
+            max_so_far = z
+            max_num = p
+    font = pygame.font.Font(None, 35)
+    msg = font.render(f"{max_num} is the max so far", True, "pink")
+    window.blit(new_display, (0,0))
+    window.blit(msg, (100,100))
+    pygame.display.flip()
+    pygame.time.wait(1000)
 
-    
-   
-font = pygame.font.Font(None, 25)
+def main():
+    upper_limit = 50
+    output = threenp1range(upper_limit)
+    graph_coordinates(output)
+    pygame.display.flip()
+    pygame.time.wait(3000)
 
-
-pygame.display.update()
-pygame.time.wait(1000)
+main()
