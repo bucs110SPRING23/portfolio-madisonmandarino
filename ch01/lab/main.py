4cb36cd3324c0001e6ed53f2a0ac544900c94143
Madison Mandarino <madisonmandarino@Madisons-MacBook-Pro.local>
2023-05-05 21:15:53 -0400

    regrade lab ch01

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,11 +1,15 @@
 #part a
 weeks = 16
+print("Number of weeks:", weeks)
 classes = 5
+print("Number of classes:", classes)
 tuition = 6000
+print("Total Tuition:", tuition)
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
-classes_per_week = int(input("How many times per week does your class meet?"))
+classes_per_week = 10
+print("Classes per week:", classes_per_week)
 cost_per_class = (cost_per_week / classes_per_week)
 print("Cost per class:", cost_per_class)
 
